6d5d93264424a44c71f4e279e03f5a8524cd681e
HTML-Worker <871921962@qq.com>
2017-05-04 23:04:37 +0800

    存入真实数据，与接口对接成功

--- app/business/modules/loginInit/loginInitCtrl.js
+++ app/business/modules/loginInit/loginInitCtrl.js
@@ -5,24 +5,61 @@
 
     loginInitCtrl.$inject = [
         "$scope",
-        "$location"
+        "$location",
+        "$http"
     ];
 
-    function loginInitCtrl($scope, $location) {
+    function loginInitCtrl($scope, $location, $http) {
 
-        $scope.loginInitChoice = true;
-        $scope.teacherRegisterView = false;
-        $scope.studentRegisterView = false;
+        $scope.loginInitChoice = false;
+        $scope.teacherRegisterView = true;
+        $scope.studentRegisterView = true;
 
-        $scope.schoolName = schoolName;
+        $scope.sex = selectData.sex;
+        $scope.schoolName = selectData.schoolName;
+        $scope.grade = selectData.grade;
+        $scope.language = selectData.language;
+        $scope.entryType = selectData.entryType;
 
         $scope.form = {
             username : "",
-            password : ""
+            password : "",
+            name : "",
+            peopleId: "",
+            teacherName : ""
         };
 
         $scope.submit = function () {
-            alert($("#sex").val());
+            $http({
+                method: "post",
+                url: "http://localhost:8080/OlympicsAPI/rest/StudentMessage/studentMessage",
+                data: {
+                    id:"",
+                    username: $scope.form.username,
+                    password: $scope.form.password,
+                    name: $scope.form.name,
+                    sex: $("#sex").val(),
+                    peopleId: $scope.form.peopleId,
+                    phone: $scope.form.phone,
+                    email: $scope.form.email,
+                    school: $("#school").val(),
+                    grade: $("#grade").val(),
+                    language: $("#language").val(),
+                    teacherName: $scope.form.teacherName,
+                    entryType: $("#entryType").val()
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).success(function (data) {
+                if ( "200" === data) {
+                    bootbox.alert("恭喜，注册成功！");
+                }else {
+                    bootbox.alert("数据录入失败，请重新输入！");
+                }
+            }).error(function (data) {
+                bootbox.alert("服务器连接失败！");
+            });
         };
         /**
          * 返回按钮绑定事件
@@ -36,7 +73,8 @@
         };
         
         $scope.studentRegister = function () {
-
+            $scope.loginInitChoice = true;
+            $scope.studentRegisterView = false;
         }
     }
 }());
